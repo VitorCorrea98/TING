5edc465857489dc8cd05132f9355bae82a744815
Vitor Correa <vrcnode@gmail.com>
2024-05-14 22:13:49 -0300

    Requisito 2 feito

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    txt = path_file.endswith("txt")
+    if not txt:
+        return sys.stderr.writelines("Formato inválido")
+    try:
+        with open(path_file) as file:
+            file_data = file.read()
+            return file_data.split("\n")
+    except FileNotFoundError:
+        return sys.stderr.writelines(f"Arquivo {path_file} não encontrado\n")
