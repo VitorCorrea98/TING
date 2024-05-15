bdc1b652b8889649b875957c29150de41d520da4
Vitor Correa <vrcnode@gmail.com>
2024-05-15 10:23:33 -0300

    Requisito 3 feito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    linhas_arquivo = txt_importer(path_file)
+    qtd_linhas = len(linhas_arquivo)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": qtd_linhas,
+        "linhas_do_arquivo": linhas_arquivo,
+    }
+    file_already_in_line = None
+    for file in instance.data:
+        if file["nome_do_arquivo"] == path_file:
+            file_already_in_line = file
+    if not file_already_in_line:
+        instance.enqueue(result)
+
+    print(result)
 
 
 def remove(instance):
