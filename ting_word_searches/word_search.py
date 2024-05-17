908611919ce85d3c56a31d02628c9594355cb3e9
Vitor Correa <vrcnode@gmail.com>
2024-05-17 16:30:32 -0300

    Requisito 7 e 8 ajustados para linter

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,19 +1,23 @@
+def support_word(word, linhas, conteudo):
+    ocorrencias = list()
+    for index, linha in enumerate(linhas):
+        for linha_word in linha.split(" "):
+            if word.lower() in linha_word.lower():
+                if conteudo:
+                    ocorrencias.append({"linha": index + 1, "conteudo": linha})
+                else:
+                    ocorrencias.append({"linha": index + 1})
+    return ocorrencias
+
+
 def find_word(word, instance, conteudo):
     lista = instance.data
     result = list()
     for arquivo_objeto in lista:
         arquivo = arquivo_objeto["nome_do_arquivo"]
         linhas = arquivo_objeto["linhas_do_arquivo"]
-        ocorrencias = list()
-        for index, linha in enumerate(linhas):
-            for linha_word in linha.split(" "):
-                if word.lower() in linha_word.lower():
-                    if conteudo:
-                        ocorrencias.append(
-                            {"linha": index + 1, "conteudo": linha}
-                        )
-                    else:
-                        ocorrencias.append({"linha": index + 1})
+        ocorrencias = support_word(word, linhas, conteudo)
+        print(ocorrencias)
         if len(ocorrencias):
             result.append(
                 {
@@ -22,6 +26,7 @@ def find_word(word, instance, conteudo):
                     "ocorrencias": ocorrencias,
                 }
             )
+
     return result
 
 
